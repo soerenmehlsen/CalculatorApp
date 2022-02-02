ecfdc44dae84b6caf4aabc5348dd915780e17246
Søren Mehlsen <202000071@post.au.dk>
2022-02-02 14:20:31 +0100

    Add project files.

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandTestCalculatorApp
+{
+    public class Calculator
+    {
+        private double _a;
+        private double _b;
+        private double _x;
+        private double _exp;
+
+
+        public Calculator()
+        {
+            _a = 0.0;
+            _b = 0.0;
+        }
+
+        public double Add(double a, double b)
+        {
+            _a = a;
+            _b = b;
+            double result = a + b;
+
+            return result;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            _a = a;
+            _b = b;
+            double result = a - b;
+
+            return result;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            _a = a;
+            _b = b;
+            double result = a * b;
+
+            return result;
+        }
+
+        public double Power(double x, double exp)
+        {
+            _x = x;
+            _exp = exp;
+            double result = Math.Pow(x, exp);
+
+            return result;
+        }
+
+
+    }
+}
---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace HandTestCalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("----Calculator App-----");
+            Console.Write("Input the first number: ");
+            double inputA = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input the second number: ");
+            double inputB = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("----Calculating-----");
+
+            Thread.Sleep(1000);
+
+            double result = calculator.Add(inputA,inputB);
+            double result2 = calculator.Subtract(inputA, inputB);
+            double result3 = calculator.Multiply(inputA, inputB);
+            double result4 = calculator.Power(inputA, inputB);
+
+            Console.WriteLine("Result of add: " + result);
+            Console.WriteLine("Result of subtract: " + result2);
+            Console.WriteLine("Result of Multiply: " + result3);
+            Console.WriteLine("Result of Power: " + result4);
+            Console.ReadLine();
+        }
+    }
+}
